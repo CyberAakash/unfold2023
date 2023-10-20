1723d591cfe9a8bc8f21337f8dd4f4663ce3ab28
CyberAakash <aakashvnth@gmail.com>
2023-10-20 09:52:09 +0530

    Homepage and Sui login to wallet integration suces

--- app/report_submit/page.js
+++ app/report_submit/page.js
@@ -1,10 +1,13 @@
-import HomePage from "@/pages/Home.Page";
+// import HomePage from "@/pages/Home.Page";
+
+import DashboardPage from "@/pages/Dashboard.Page";
 
 export default function Home() {
   return (
     <>
       <div className="m-0 px-3 py-0 box-border flex items-center justify-center">
-        <HomePage />
+        {/* <HomePage /> */}
+        <DashboardPage />
       </div>
     </>
   );
---
+++ app/store_data_in_wallet/page.js
@@ -0,0 +1,14 @@
+// import HomePage from "@/pages/Home.Page";
+
+import SubmitReportPage from "@/pages/SubmitReport.Page";
+
+export default function Home() {
+  return (
+    <>
+      <div className="m-0 px-3 py-0 box-border flex items-center justify-center bg-bg1">
+        {/* <HomePage /> */}
+        <SubmitReportPage />
+      </div>
+    </>
+  );
+}
